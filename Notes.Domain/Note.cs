2742d4e33618a1c6d891ea9de2d21778b93d7e2c
Gandoler <gl.krutoi@mail.ru>
2024-12-31 12:20:20 +0300

    Начало Разметки

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Notes.Domain
+{
+    internal class Note
+    { 
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty ;
+        public string Details { get; set; } = string.Empty ;
+
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+
+    }
+}
